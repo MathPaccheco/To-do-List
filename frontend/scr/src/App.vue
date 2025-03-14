57dc5b16fc3a6dc9ccb13c134ed31c16b455df43
Matheus Vieira Mendes Pacheco <131549317+MathPaccheco@users.noreply.github.com>
2025-03-14 15:25:04 -0300

    Add files via upload

---
+++ frontend/scr/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import TaskList from "./components/TaskList.vue";
+</script>
+
+<template>
+  <div class="container">
+    <h1>Gerenciador de Tarefas</h1>
+    <TaskList />
+  </div>
+</template>
+
+<style>
+.container {
+  max-width: 600px;
+  margin: auto;
+  text-align: center;
+  font-family: Arial, sans-serif;
+}
+</style>
